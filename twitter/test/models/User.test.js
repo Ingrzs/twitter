ebfa8694ddc5aa1cfecb29c1eb5d7c032d667eba
Ingrzs <ing.ramirozs@gmail.com>
2022-04-21 18:51:54 -0500

    añadiendo las pruebas para los getters

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -11,4 +11,15 @@ expect(usar.bio).toBe("bio");
 expect(usar.dateCreated).not.toBeUndefined();
 expect(usar.lastUpdate).not.toBeUndefined();
 })
+
+
+test('prueba para los metodos', () =>{
+const usar = new User(1,"ramiro","rzs","bio");
+expect(usar.getusername).toBe("ramiro");
+expect(usar.getbio).toBe("bio");
+expect(usar.getDateCreated).not.toBeUndefined();
+expect(usar.getLastUpdated).not.toBeUndefined();
+})
+
+
 })
