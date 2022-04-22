707833d7121829bd1ce3bc5ddacd1928c0a635cc
Ingrzs <ing.ramirozs@gmail.com>
2022-04-21 19:03:43 -0500

    prueba para los setters

--- twitter/test/models/User.test.js
+++ twitter/test/models/User.test.js
@@ -4,22 +4,30 @@ describe ("unit test class", () => {
   test ('crear objeto', () => {
   const usar = new User(1, "ramiro", "rzs", "bio")
 
-expect(usar.id).toBe(1);
-expect(usar.username).toBe("ramiro");
-expect(usar.name).toBe("rzs");
-expect(usar.bio).toBe("bio");
-expect(usar.dateCreated).not.toBeUndefined();
-expect(usar.lastUpdate).not.toBeUndefined();
-})
+  expect(usar.id).toBe(1);
+  expect(usar.username).toBe("ramiro");
+  expect(usar.name).toBe("rzs");
+  expect(usar.bio).toBe("bio");
+  expect(usar.dateCreated).not.toBeUndefined();
+  expect(usar.lastUpdate).not.toBeUndefined();
+  })
 
 
-test('prueba para los metodos', () =>{
-const usar = new User(1,"ramiro","rzs","bio");
-expect(usar.getusername).toBe("ramiro");
-expect(usar.getbio).toBe("bio");
-expect(usar.getDateCreated).not.toBeUndefined();
-expect(usar.getLastUpdated).not.toBeUndefined();
-})
+  test('prueba para los getters', () =>{
+  const usar = new User(1,"ramiro","rzs","bio");
+  expect(usar.getusername).toBe("ramiro");
+  expect(usar.getbio).toBe("bio");
+  expect(usar.getDateCreated).not.toBeUndefined();
+  expect(usar.getLastUpdated).not.toBeUndefined();
+  })
 
+  test('prueba para los setters',()=>{
+      const usar = new User(1,"ramiro","rzs","bio")
+      usar.setusername = "ramirozs"
+      expect(usar.username).toBe("ramirozs")
+
+      usar.setbio = "bio new"
+      expect(usar.bio).toBe("bio new")
+  })
 
 })
