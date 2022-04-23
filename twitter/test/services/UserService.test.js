2ac4da39cc37542a85b28ef05368b17415e2e1b5
Ingrzs <ing.ramirozs@gmail.com>
2022-04-23 15:23:58 -0500

    añadiendo prueba de requerimento 2

--- twitter/test/services/UserService.test.js
+++ twitter/test/services/UserService.test.js
@@ -9,4 +9,18 @@ describe("Test para usuarios del servicio",()=>{
     expect(user.id).toBe(1)
     expect(user.bio).not.toBeUndefined()
     })
+
+
+   test('Requerimiento dos',() =>{
+       const user = UserServices.create(1,"ramirozs", "rzs")
+       const userlista = UserServices.getInfo(user)
+       expect(userlista[0]).toBe(1)  
+       expect(userlista[1]).toBe("ramirozs")
+       expect(userlista[2]).toBe("rzs")
+       expect(userlista[3]).toBe("sinbio") 
+   })
+
+
+  
+
 } )
