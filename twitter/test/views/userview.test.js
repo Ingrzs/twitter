f534899d5a270eddb9f8076c13c46cd93687e9d2
Ingrzs <ing.ramirozs@gmail.com>
2022-04-23 20:06:18 -0500

    agregando prueba para validar el tipo de datos

--- twitter/test/views/userview.test.js
+++ twitter/test/views/userview.test.js
@@ -7,4 +7,10 @@ test('requerimiento uno vista',()=>{
     expect(result.error).toMatch(/payload no existe/)
 })
 
+test("Requerimiento dos", () => {
+    const payload = {username: null, name: 12, id: "id"}
+    const result = UserView.createUser(payload)
+    expect(result.error).toMatch(/necesitan tener un valor válido/)
+  })
+
 })
